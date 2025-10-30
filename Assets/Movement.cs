ba605fb1590180f5b18a20117c511575eaf260c8
laithooo <laithghannam5@gmail.com>
2025-10-30 18:16:19 -0400

    added basics

---
+++ Assets/Movement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public float speed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, 0, vertical) * speed;
+
+        if (horizontal!=0 || vertical != 0)
+        {
+            transform.Translate(movement);
+        }
+    }
+
+    
+}
